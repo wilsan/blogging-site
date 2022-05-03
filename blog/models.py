10fd2466b50b550e04cab1c220a64a8864f627d7
wilson <wilsongeorge1996@gmail.com>
2022-05-03 23:59:26 +0530

    Add Models to store post data

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    slug = models.SlugField(unique=True, db_index=True)
+    # for SlugField() db_index is True by default
+    # setting uniqie=True also automatically sets db_index=True
+    
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
